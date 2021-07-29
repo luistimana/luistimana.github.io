4554921c2b2e6a0a1e323207ba644a3ef548d318
Luis Timaná <60704967+luistimana@users.noreply.github.com>
2021-07-29 09:22:14 -0500

    Initial commit

---
+++ layouts/derecha.php
@@ -0,0 +1,134 @@
+	<div class="col-lg-4">
+								
+								<div class="categori-widget">
+									<h4 class="title">
+										Categorias
+									</h4>
+									<ul class="cat-list">
+										<li>
+											<a href="#">
+												<p>
+													Web
+												</p>
+												<span class="count">
+												33
+												</span>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p>
+													Movil
+												</p>
+												<span class="count">
+													77
+												</span>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p>
+													Diseño Gráfico
+												</p>
+												<span class="count">
+													49
+												</span>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p>
+													Desktop
+												</p>
+												<span class="count">
+													70
+												</span>
+											</a>
+										</li>
+										<!-- <li>
+											<a href="#">
+												<p>
+													Crypto
+												</p>
+												<span class="count">
+													33
+												</span>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<p>
+													Lending
+												</p>
+												<span class="count">
+													38
+												</span>
+											</a>
+										</li> -->
+									</ul>
+								</div>
+								<!-- <div class="latest-post-widget">
+									<h4 class="title">
+										Latest Post
+									</h4>
+									<ul class="post-list">
+										<li>
+											<div class="post">
+												<div class="post-img">
+													<img src="assets/images/blog/img1.png" alt="">
+												</div>
+												<div class="post-details">
+													<a href="#" class="post-title">
+														5 reasons why you website  
+													</a>
+												</div>
+											</div>
+										</li>
+										<li>
+											<div class="post">
+												<div class="post-img">
+													<img src="assets/images/blog/img2.png" alt="">
+												</div>
+												<div class="post-details">
+													<a href="#" class="post-title">
+														6 reasons why you website 
+													</a>
+												</div>
+											</div>
+										</li>
+										<li>
+											<div class="post">
+												<div class="post-img">
+													<img src="assets/images/blog/img3.png" alt="">
+												</div>
+												<div class="post-details">
+													<a href="#" class="post-title">
+														7 reasons why you website 
+													</a>
+												</div>
+											</div>
+										</li>
+										<li>
+											<div class="post">
+												<div class="post-img">
+													<img src="assets/images/blog/img4.png" alt="">
+												</div>
+												<div class="post-details">
+													<a href="#" class="post-title">
+														8 reasons why you website 
+													</a>
+												</div>
+											</div>
+										</li>
+									</ul>
+								</div>
+								<div class="newsletter-widget">
+									<h4 class="title">
+											NewsLetter
+									</h4>
+									<form action="#" class="contact-form">
+										<input type="text" class="form-control" placeholder="Enter Email..">
+										<button type="submit" class="mybtn3 mybtn-bg mt-4">Subscribe</button>
+									</form> 
+								</div> -->
+							</div>
